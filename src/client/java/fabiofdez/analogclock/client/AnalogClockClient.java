523e88e10f803c4db1e2b9df873d2a67da6dadba
Fabio Fernandez <ffernan9@jhu.edu>
2026-04-03 14:56:04 -0500

    initial commit w/ mod template

---
+++ src/client/java/fabiofdez/analogclock/client/AnalogClockClient.java
@@ -0,0 +1,10 @@
+package fabiofdez.analogclock.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AnalogClockClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
