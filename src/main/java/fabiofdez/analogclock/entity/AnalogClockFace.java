f6bbbd3ec9ff7d703e13c0d68b31ad75897e76da
Fabio Fernandez <ffernan9@jhu.edu>
2026-04-07 21:31:36 -0500

    v1.1.0 - animate clock time set > auto-set clock time when placed > clock hands animate into position > add whirring wind-up sound effects + refactor frame calculation & format

--- src/main/java/fabiofdez/analogclock/entity/AnalogClockFace.java
+++ src/main/java/fabiofdez/analogclock/entity/AnalogClockFace.java
@@ -1,6 +1,7 @@
 package fabiofdez.analogclock.entity;
 
 import fabiofdez.analogclock.ModBlockEntities;
+import fabiofdez.analogclock.util.ClockHandsInterpolator;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
@@ -8,6 +9,8 @@ import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -15,44 +18,38 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class AnalogClockFace extends BlockEntity {
-  private int lastHourFrame = 0;
-  private int lastMinuteFrame = 0;
+  public static final long DAY_LENGTH_TICKS = 24000;
+  public static final long HALF_DAY_LENGTH_TICKS = DAY_LENGTH_TICKS / 2;
+  public static final long HOUR_LENGTH_TICKS = DAY_LENGTH_TICKS / 24;
+  public static final long SUNRISE_TICK_OFFSET = 6000; // 6AM = dayTime 0
+
+  public static final int CLOCK_HAND_FRAMES = 24;
+  public static final int UNIT_HOUR_FRAMES = 2;
+  public static final int HOUR_FRAMES_RADIX = 12;
+  public static final int NUM_CLOCK_FRAMES = CLOCK_HAND_FRAMES * HOUR_FRAMES_RADIX;
+
+  private static final int INITIAL_CLOCK_FRAME = 3 * UNIT_HOUR_FRAMES * HOUR_FRAMES_RADIX; // 3:00
+
+  private final ClockHandsInterpolator ANIMATOR;
+  private int currentFrame;
 
   public AnalogClockFace(BlockPos blockPos, BlockState blockState) {
     super(ModBlockEntities.CLOCK_FACE_ENTITY, blockPos, blockState);
-    if (level == null) return;
 
-    long dayTime = level.getDayTime();
-    lastHourFrame = calculateHour(dayTime);
-    lastMinuteFrame = calculateMinute(dayTime);
+    currentFrame = INITIAL_CLOCK_FRAME;
+    ANIMATOR = new ClockHandsInterpolator();
   }
 
   public static void tick(Level level, BlockPos pos, BlockState state, BlockEntity entity) {
-    if (level.isClientSide) return;
+    if (level.isClientSide || !(entity instanceof AnalogClockFace clockFace)) return;
 
-    AnalogClockFace clockFace = (AnalogClockFace) entity;
+    if (level.dimension() != Level.OVERWORLD) return; // TODO: chaotic wobble?
 
-    // day length = 24000
-    // minute frame length = 1000 / 24 ~ 41.6 ticks
-    // hour frame length = 12000 / 24 = 500 ticks
+    int nextFrame = getNextFrame(level, clockFace);
+    if (nextFrame == clockFace.currentFrame) return;
 
-    // Time conversions:
-    //       0 == 06:00 AM
-    //    1000 == 07:00 AM
-    //    1500 == 07:30 AM
-    //    6000 == 12:00 PM
-    //   12000 == 06:00 PM
-    //   18000 == 12:00 AM
-
-    long dayTime = level.getDayTime();
-    int hourFrame = calculateHour(dayTime);
-    int minuteFrame = calculateMinute(dayTime);
-
-    if (hourFrame == clockFace.lastHourFrame && minuteFrame == clockFace.lastMinuteFrame) return;
-
-    clockFace.lastHourFrame = hourFrame;
-    clockFace.lastMinuteFrame = minuteFrame;
-
+    if (clockFace.ANIMATOR.inProgress()) playWindUpTick(clockFace, level, pos);
+    clockFace.currentFrame = nextFrame;
     setChanged(level, pos, state);
 
     ((ServerLevel) level)
@@ -60,28 +57,62 @@ public class AnalogClockFace extends BlockEntity {
         .blockChanged(pos);
   }
 
-  private static int calculateHour(long dayTime) {
-    long hourTickOffset = (dayTime % 12000) + 6000;
-    return (int) (hourTickOffset / 500) % 24;
-  }
-
-  private static int calculateMinute(long dayTime) {
-    long minuteTickOffset = (dayTime % 1000) * 24;
-    return (int) (minuteTickOffset / 1000) % 24;
-  }
-
   public int getHourFrame() {
-    return lastHourFrame;
+    return (currentFrame / HOUR_FRAMES_RADIX) % NUM_CLOCK_FRAMES;
   }
 
   public int getMinuteFrame() {
-    return lastMinuteFrame;
+    int hourPart = (currentFrame / HOUR_FRAMES_RADIX) % UNIT_HOUR_FRAMES;
+    return (currentFrame % HOUR_FRAMES_RADIX) + (hourPart * HOUR_FRAMES_RADIX);
+  }
+
+  private static int getNextFrame(Level level, AnalogClockFace clockFace) {
+    ClockHandsInterpolator animator = clockFace.ANIMATOR;
+    long dayTime = level.getDayTime();
+    int nextFrame;
+
+    if (animator.inProgress()) {
+      if (animator.progress() >= 0.75F) {
+        animator.interp(clockFace.currentFrame, toClockFrame(dayTime));
+      }
+      return animator.step();
+    }
+
+    nextFrame = toClockFrame(dayTime);
+
+    if (!animator.isInitialized()) {
+      animator.interp(clockFace.currentFrame, nextFrame);
+      return animator.step();
+    }
+
+    return nextFrame;
+  }
+
+  private static int toClockFrame(long dayTime) {
+    long clockTime = (dayTime + SUNRISE_TICK_OFFSET) % HALF_DAY_LENGTH_TICKS;
+    int frameOffset = Math.toIntExact((clockTime * CLOCK_HAND_FRAMES) / HOUR_LENGTH_TICKS);
+
+    return frameOffset % NUM_CLOCK_FRAMES;
+  }
+
+  private static void playWindUpTick(AnalogClockFace clockFace, Level level, BlockPos pos) {
+    if (level == null || clockFace == null) return;
+
+    float chance = level
+        .getRandom()
+        .nextFloat();
+
+    if (chance / clockFace.ANIMATOR.speed() > 0.5F) return;
+
+    float pitch = chance >= 0.75F ? 1.2F : 1.4F;
+    level.playSound(null, pos, SoundEvents.SPYGLASS_USE, SoundSource.BLOCKS, 0.5F, pitch);
   }
 
   @Override
   protected void saveAdditional(CompoundTag nbt, HolderLookup.Provider registryLookup) {
-    nbt.putInt("hourFrame", lastHourFrame);
-    nbt.putInt("minuteFrame", lastMinuteFrame);
+    if (ANIMATOR.isInitialized()) {
+      nbt.putInt("clockFrame", currentFrame);
+    }
 
     super.saveAdditional(nbt, registryLookup);
   }
@@ -91,11 +122,8 @@ public class AnalogClockFace extends BlockEntity {
     super.loadAdditional(nbt, registryLookup);
 
     nbt
-        .getInt("hourFrame")
-        .ifPresent((num) -> lastHourFrame = num);
-    nbt
-        .getInt("minuteFrame")
-        .ifPresent((num) -> lastMinuteFrame = num);
+        .getInt("clockFrame")
+        .ifPresent((num) -> currentFrame = num);
   }
 
   @Override
