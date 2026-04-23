cfe5b5a5284b73f64e9644fcdba7865898ab1a59
Fabio Fernandez <ffernan9@jhu.edu>
2026-04-23 00:07:41 -0500

    add stonecutter for multi-version > move to kts build/settings files > version 1.21.5 fully set up, working

---
+++ build.gradle.kts
@@ -0,0 +1,102 @@
+plugins {
+    id("net.fabricmc.fabric-loom-remap")
+}
+
+version = property("mod_version") as String
+group = property("maven_group") as String
+
+repositories {
+    // Add repositories to retrieve artifacts from in here.
+    // You should only use this when depending on other mods because
+    // Loom adds the essential maven repositories to download Minecraft and libraries from automatically.
+    // See https://docs.gradle.org/current/userguide/declaring_repositories.html
+    // for more information about repositories.
+}
+
+loom {
+    splitEnvironmentSourceSets()
+
+    mods {
+        register("analog-clock") {
+            sourceSet(sourceSets.main.get())
+            sourceSet(sourceSets.getByName("client"))
+        }
+    }
+}
+
+fabricApi {
+    configureDataGeneration {
+        client = true
+    }
+}
+
+var minecraftVersion = property("minecraft_version") as String
+
+dependencies {
+    minecraft("com.mojang:minecraft:${property("minecraft_version")}")
+    mappings(loom.officialMojangMappings())
+    modImplementation("net.fabricmc:fabric-loader:${property("loader_version")}")
+
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_api_version")}")
+}
+
+tasks.processResources {
+    inputs.property("version", project.version)
+    inputs.property("minecraft", minecraftVersion)
+
+    var props = mapOf(
+        "version" to project.version, "minecraft" to minecraftVersion
+    )
+
+    filesMatching("fabric.mod.json") { expand(props) }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release = 21
+}
+
+java {
+    // Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
+    // if it is present.
+    // If you remove this line, sources will not be generated.
+    withSourcesJar()
+
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.jar {
+    inputs.property("projectName", project.name)
+
+    from("LICENSE") {
+        rename { "${it}_${project.name}" }
+    }
+}
+
+// configure build artifact files
+var jarName = "${property("artifact_name")}-${project.version}+mc${minecraftVersion}"
+
+tasks.remapJar {
+    archiveFileName = "${jarName}.jar"
+}
+
+tasks.remapSourcesJar {
+    archiveFileName = "${jarName}-sources.jar"
+}
+
+// configure the maven publication
+//publishing {
+//    publications {
+//        register<MavenPublication>("mavenJava") {
+//            from(components["java"])
+//        }
+//    }
+//
+//    // See https://docs.gradle.org/current/userguide/publishing_maven.html for information on how to set up publishing.
+//    repositories {
+//        // Add repositories to publish to here.
+//        // Notice: This block does NOT have the same function as the block in the top level.
+//        // The repositories here will be used for publishing your artifact, not for
+//        // retrieving dependencies.
+//    }
+//}
---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.kikugie.dev/snapshots") { name = "KikuGie Snapshots" }
+    }
+
+    plugins {
+        id("net.fabricmc.fabric-loom-remap") version "1.15-SNAPSHOT"
+    }
+}
+
+plugins {
+    id("dev.kikugie.stonecutter") version "0.9.2"
+}
+
+stonecutter {
+    create(rootProject) {
+        versions("1.21.5")
+        vcsVersion = "1.21.5"
+    }
+}
+
+// Should match your modid
+rootProject.name = "analog-clock"
