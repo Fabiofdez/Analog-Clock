a67f13fce21a9fc51553e97f34b094ef13a31843
Fabio Fernandez <ffernan9@jhu.edu>
2026-04-24 18:45:23 -0500

    minor improvements to build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,28 +2,25 @@ plugins {
     id("net.fabricmc.fabric-loom-remap")
 }
 
-version = property("mod_version") as String
-group = property("maven_group") as String
+version = "${property("mod_version")}"
+group = "${property("maven_group")}"
 
-var modId = property("mod_id") as String
-var displayName = property("display_name") as String
-var minecraftVersion = property("minecraft_version") as String
-var loaderVersion = property("loader_version") as String
+var minecraftVersion = "${property("minecraft_version")}"
+var fabricApiVersion = "${property("fabric_api_version")}"
+var fabricLoaderVersion = "${property("loader_version")}"
+
+var modId = "${property("mod_id")}"
+var displayName = "${property("display_name")}"
 var jarName = "${modId}-${project.version}+mc${minecraftVersion}"
 
 repositories {
-    // Add repositories to retrieve artifacts from in here.
-    // You should only use this when depending on other mods because
-    // Loom adds the essential maven repositories to download Minecraft and libraries from automatically.
-    // See https://docs.gradle.org/current/userguide/declaring_repositories.html
-    // for more information about repositories.
 }
 
 loom {
     splitEnvironmentSourceSets()
 
     mods {
-        register("analog-clock") {
+        register(modId) {
             sourceSet(sourceSets.main.get())
             sourceSet(sourceSets.getByName("client"))
         }
@@ -39,20 +36,16 @@ fabricApi {
 dependencies {
     minecraft("com.mojang:minecraft:${minecraftVersion}")
     mappings(loom.officialMojangMappings())
-    modImplementation("net.fabricmc:fabric-loader:${loaderVersion}")
-
-    modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_api_version")}")
+    modImplementation("net.fabricmc:fabric-loader:${fabricLoaderVersion}")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${fabricApiVersion}")
 }
 
 tasks.processResources {
-    inputs.property("version", project.version)
-    inputs.property("minecraft", minecraftVersion)
-
     var props = mapOf(
         "id" to modId,
         "version" to project.version,
         "name" to displayName,
-        "loader_version" to loaderVersion,
+        "loader_version" to fabricLoaderVersion,
         "minecraft" to minecraftVersion
     )
 
